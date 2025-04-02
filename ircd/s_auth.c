6bdce3bacd6e452798354d93a76f93cea0648a03
WarPigs1602 <gh@midiandmore.de>
2025-04-02 12:46:05 +0200

    Update s_auth.c

--- ircd/s_auth.c
+++ ircd/s_auth.c
@@ -1967,6 +1967,14 @@ static int iauth_cmd_sasl(struct IAuth *iauth, struct Client *cli,
         cli_user(cli)->acc_create = atoi(timestamp);
 	    cli_user(cli)->acc_id = strtoul(id, NULL, 10);
 		SetAccount(cli);
+		ircd_snprintf(0, cli_user(cli)->authhost, HOSTLEN, "%s.%s", account, feature_str(FEAT_HIDDEN_HOST));
+		int killreason = find_kill(cli,1);	
+	    if (killreason)
+	    {
+		  ++ServerStats->is_ref;
+		  return exit_client(cli, cli, &me,
+							(killreason == -1 ? "K-lined" : "G-lined"));
+	    }		
 		send_reply(cli, RPL_LOGGEDIN, cli, cli_name(cli), account);
 		send_reply(cli, RPL_SASLSUCCESS);
 	} else if(!ircd_strcmp(cmd, "N")) {
